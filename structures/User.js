00f3b02366afc7f36eca2b9dd9b949d01a585b41
itxreyanon <itxreyanon@gmail.com>
2025-07-25 13:53:33 +0500

    Update User.js

--- structures/User.js
+++ structures/User.js
@@ -1,244 +1,322 @@
-const Collection = require('@discordjs/collection')
+
+import { Collection } from './Collection.js';
 
 /**
- * Represents a User
+ * Represents an Instagram user with enhanced functionality
  */
-class User {
+export class User {
+  constructor(client, data) {
     /**
-     * @param {Client} client The instantiating client
-     * @param {object} data The data for the user
+     * The client that instantiated this user
+     * @type {Client}
      */
-    constructor (client, data) {
-        /**
-         * @type {Client}
-         * The client that instantiated this
-         */
-        this.client = client
-        /**
-         * @type {string}
-         * The ID of the user
-         */
-        this.id = data.pk
-        /**
-         * @type {Collection<string, User>}
-         * Collection of users that follow this user.
-         * <info>You have to use user.fetchFollowers() to fill the collection</info>
-         */
-        this.followers = new Collection()
-        /**
-         * @type {Collection<string, User>}
-         * Collection of users this user follows.
-         * <info>You have to use user.fetchFollowing() to fill the collection</info>
-         */
-        this.following = new Collection()
-
-        this._patch(data)
-    }
+    this.client = client;
 
     /**
-     * @type {Chat}
-     * Private chat between the client and the user.
+     * The user's ID
+     * @type {string}
      */
-    get privateChat () {
-        return this.client.cache.chats.find((chat) => chat.users.size === 1 && chat.users.first().id === this.id)
-    }
-
-    _patch (data) {
-        /**
-         * @type {string}
-         * The username of the user
-         */
-        this.username = 'username' in data ? data.username : this.username
-        /**
-         * @type {string}
-         * The full name of the user
-         */
-        this.fullName = 'full_name' in data ? data.full_name : this.fullName
-        /**
-         * @type {boolean}
-         * Whether the user is private
-         */
-        this.isPrivate = 'is_private' in data ? data.is_private : this.isPrivate
-        /**
-         * @type {boolean}
-         * Whether the user is verified
-         */
-        this.isVerified = 'is_verified' in data ? data.is_verified : this.isVerified
-        /**
-         * @type {boolean}
-         * Whether the user is a business profile
-         */
-        this.isBusiness = 'is_business' in data ? data.is_business : this.isBusiness
-        /**
-         * @type {string}
-         * The URL of the user's avatar
-         */
-        this.avatarURL = 'profile_pic_url' in data ? data.profile_pic_url : this.avatarURL
-        /**
-         * @type {string?}
-         * The biography of the user
-         */
-        this.biography = 'biography' in data ? data.biography : this.biography
-        /**
-         * @type {number?}
-         * The number of media published by the user
-         */
-        this.mediaCount = 'media_count' in data ? data.media_count : this.mediaCount
-        /**
-         * @type {number?}
-         * The number of followers of the user
-         */
-        this.followerCount = 'follower_count' in data ? data.follower_count : this.followerCount
-        /**
-         * @type {number?}
-         * The number of followed users by the user
-         */
-        this.followingCount = 'following_count' in data ? data.following_count : this.followingCount
-        /**
-         * @type {number?}
-         * The number of videos published by IGTV
-         */
-        this.totalIgtvVideos = 'total_igtv_videos' in data ? data.total_igtv_videos : this.totalIgtvVideos
-    }
+    this.id = data.pk || data.id;
 
     /**
-     * Fetch the user to access all the properties
-     * @returns {Promise<User>}
+     * Collection of users that follow this user
+     * @type {Collection<string, User>}
      */
-    async fetch () {
-        const user = await this.client.fetchUser(this.id, true)
-        return user
-    }
+    this.followers = new Collection();
 
     /**
-     * Fetch (or create) a private chat between the client and the user.
-     * @returns {Promise<Chat>}
-     * @example
-     * // Send a message to @selenagomez
-     * const user = await client.fetchUser('selenagomez');
-     * // Use fetchPrivateChat() as I've never sent a message to this account
-     * if(!user.privateChat) await user.fetchPrivateChat();
-     * // Then I can use User#privateChat
-     * user.privateChat.sendPhoto('https://picsum.photos/536/354');
+     * Collection of users this user follows
+     * @type {Collection<string, User>}
      */
-    async fetchPrivateChat () {
-        if (this.privateChat) return this.privateChat
-        const chat = await this.client.createChat([ this.id ])
-        return chat
-    }
+    this.following = new Collection();
+
+    this._patch(data);
+  }
+
+  /**
+   * The private chat between the client and this user
+   * @type {Chat|null}
+   */
+  get privateChat() {
+    return this.client.cache.chats.find(chat => 
+      chat.users.size === 1 && chat.users.first().id === this.id
+    );
+  }
+
+  /**
+   * Whether this user is the bot itself
+   * @type {boolean}
+   */
+  get isBot() {
+    return this.id === this.client.user?.id;
+  }
+
+  /**
+   * Update user data
+   * @param {Object} data - User data from Instagram API
+   * @private
+   */
+  _patch(data) {
+    /**
+     * The user's username
+     * @type {string}
+     */
+    this.username = data.username ?? this.username;
 
     /**
-     * Fetch the users that follow this user
-     * @returns {Promise<Collection<string, User>>}
+     * The user's full name
+     * @type {string}
      */
-    async fetchFollowers () {
-        const followersItems = await this.client.ig.feed.accountFollowers(this.id).items()
-        followersItems.forEach((user) => {
-            this.followers.set(user.pk, this.client._patchOrCreateUser(user.pk, user))
-        })
-        return this.followers
-    }
+    this.fullName = data.full_name ?? this.fullName;
 
     /**
-     * Fetch the users that follow this user
-     * @returns {Promise<Collection<string, User>>}
+     * Whether the user's account is private
+     * @type {boolean}
      */
-    async fetchFollowing () {
-        const followingItem = await this.client.ig.feed.accountFollowing(this.id).items()
-        followingItem.forEach((user) => {
-            this.following.set(user.pk, this.client._patchOrCreateUser(user.pk, user))
-        })
-        return this.following
-    }
+    this.isPrivate = data.is_private ?? this.isPrivate;
 
     /**
-     * Start following a user
-     * @returns {Promise<void>}
+     * Whether the user is verified
+     * @type {boolean}
      */
-    async follow () {
-        await this.client.ig.friendship.create(this.id)
-    }
+    this.isVerified = data.is_verified ?? this.isVerified;
 
     /**
-     * Stop following a user
-     * @returns {Promise<void>}
+     * Whether the user has a business account
+     * @type {boolean}
      */
-    async unfollow () {
-        await this.client.ig.friendship.destroy(this.id)
-    }
+    this.isBusiness = data.is_business ?? this.isBusiness;
 
     /**
-     * Block a user
-     * @returns {Promise<void>}
+     * The user's profile picture URL
+     * @type {string}
      */
-    async block () {
-        await this.client.ig.friendship.block(this.id)
-    }
+    this.avatarURL = data.profile_pic_url ?? this.avatarURL;
 
     /**
-     * Unblock a user
-     * @returns {Promise<void>}
+     * The user's biography
+     * @type {string|null}
      */
-    async unblock () {
-        await this.client.ig.friendship.unblock(this.id)
-    }
+    this.biography = data.biography ?? this.biography;
 
     /**
-     * Approve follow request
-     * @returns {Promise<void>}
+     * Number of posts by the user
+     * @type {number|null}
      */
-    async approveFollow () {
-        await this.client.ig.friendship.approve(this.id)
-    }
+    this.mediaCount = data.media_count ?? this.mediaCount;
 
     /**
-     * Reject follow request
-     * @returns {Promise<void>}
+     * Number of followers
+     * @type {number|null}
      */
-    async denyFollow () {
-        await this.client.ig.friendship.deny(this.id)
-    }
+    this.followerCount = data.follower_count ?? this.followerCount;
 
     /**
-     * Remove the user from your followers
-     * @returns {Promise<void>}
+     * Number of users this user follows
+     * @type {number|null}
      */
-    async removeFollower () {
-        await this.client.ig.friendship.removeFollower(this.id)
-    }
+    this.followingCount = data.following_count ?? this.followingCount;
 
     /**
-     * Send a message to the user
-     * @param {string} content The content of the message to send
-     * @returns {Promise<Message>}
+     * Timestamp when user was last seen
+     * @type {Date|null}
      */
-    async send (content) {
-        if (!this.privateChat) await this.fetchPrivateChat()
-        return await this.privateChat.send(content)
-    }
+    this.lastSeen = data.last_seen ? new Date(data.last_seen * 1000) : this.lastSeen;
+  }
 
-    toString () {
-        return this.id
-    }
+  /**
+   * Fetch fresh user data from Instagram
+   * @param {boolean} force - Whether to force fetch even if cached
+   * @returns {Promise<User>}
+   */
+  async fetch(force = false) {
+    return await this.client.fetchUser(this.id, force);
+  }
 
-    toJSON () {
-        return {
-            client: this.client.toJSON(),
-            username: this.username,
-            fullName: this.fullName,
-            isPrivate: this.isPrivate,
-            isVerified: this.isVerified,
-            isBusiness: this.isBusiness,
-            avatarURL: this.avatarURL,
-            biography: this.biography,
-            mediaCount: this.mediaCount,
-            followerCount: this.followerCount,
-            followingCount: this.followingCount,
-            followers: this.followers.map((u) => u.id),
-            following: this.following.map((u) => u.id),
-            totalIgtvVideos: this.totalIgtvVideos
-        }
+  /**
+   * Get or create a private chat with this user
+   * @returns {Promise<Chat>}
+   */
+  async createPrivateChat() {
+    if (this.privateChat) return this.privateChat;
+    return await this.client.createChat([this.id]);
+  }
+
+  /**
+   * Send a message to this user
+   * @param {string} content - Message content
+   * @returns {Promise<Message>}
+   */
+  async send(content) {
+    const chat = await this.createPrivateChat();
+    return await chat.sendMessage(content);
+  }
+
+  /**
+   * Send a photo to this user
+   * @param {string|Buffer|Attachment} attachment - Photo to send
+   * @returns {Promise<Message>}
+   */
+  async sendPhoto(attachment) {
+    const chat = await this.createPrivateChat();
+    return await chat.sendPhoto(attachment);
+  }
+
+  /**
+   * Follow this user
+   * @returns {Promise<void>}
+   */
+  async follow() {
+    if (this.isBot) throw new Error('Cannot follow yourself');
+    await this.client.ig.friendship.create(this.id);
+  }
+
+  /**
+   * Unfollow this user
+   * @returns {Promise<void>}
+   */
+  async unfollow() {
+    if (this.isBot) throw new Error('Cannot unfollow yourself');
+    await this.client.ig.friendship.destroy(this.id);
+  }
+
+  /**
+   * Block this user
+   * @returns {Promise<void>}
+   */
+  async block() {
+    if (this.isBot) throw new Error('Cannot block yourself');
+    await this.client.ig.friendship.block(this.id);
+  }
+
+  /**
+   * Unblock this user
+   * @returns {Promise<void>}
+   */
+  async unblock() {
+    await this.client.ig.friendship.unblock(this.id);
+  }
+
+  /**
+   * Approve follow request from this user
+   * @returns {Promise<void>}
+   */
+  async approveFollow() {
+    await this.client.ig.friendship.approve(this.id);
+  }
+
+  /**
+   * Deny follow request from this user
+   * @returns {Promise<void>}
+   */
+  async denyFollow() {
+    await this.client.ig.friendship.deny(this.id);
+  }
+
+  /**
+   * Remove this user from followers
+   * @returns {Promise<void>}
+   */
+  async removeFollower() {
+    await this.client.ig.friendship.removeFollower(this.id);
+  }
+
+  /**
+   * Fetch users that follow this user
+   * @returns {Promise<Collection<string, User>>}
+   */
+  async fetchFollowers() {
+    const followers = await this.client.ig.feed.accountFollowers(this.id).items();
+    this.followers.clear();
+    
+    for (const follower of followers) {
+      const user = this.client._patchOrCreateUser(follower.pk, follower);
+      this.followers.set(user.id, user);
     }
+    
+    return this.followers;
+  }
+
+  /**
+   * Fetch users this user follows
+   * @returns {Promise<Collection<string, User>>}
+   */
+  async fetchFollowing() {
+    const following = await this.client.ig.feed.accountFollowing(this.id).items();
+    this.following.clear();
+    
+    for (const user of following) {
+      const userObj = this.client._patchOrCreateUser(user.pk, user);
+      this.following.set(userObj.id, userObj);
+    }
+    
+    return this.following;
+  }
+
+  /**
+   * Check if this user follows another user
+   * @param {string|User} user - User to check
+   * @returns {Promise<boolean>}
+   */
+  async isFollowing(user) {
+    const userId = typeof user === 'string' ? user : user.id;
+    const friendship = await this.client.ig.friendship.show(userId);
+    return friendship.following;
+  }
+
+  /**
+   * Check if this user is followed by another user
+   * @param {string|User} user - User to check
+   * @returns {Promise<boolean>}
+   */
+  async isFollowedBy(user) {
+    const userId = typeof user === 'string' ? user : user.id;
+    const friendship = await this.client.ig.friendship.show(userId);
+    return friendship.followed_by;
+  }
+
+  /**
+   * Get display name (full name or username)
+   * @returns {string}
+   */
+  get displayName() {
+    return this.fullName || this.username;
+  }
+
+  /**
+   * Get user mention string
+   * @returns {string}
+   */
+  get mention() {
+    return `@${this.username}`;
+  }
+
+  /**
+   * String representation
+   * @returns {string}
+   */
+  toString() {
+    return this.mention;
+  }
+
+  /**
+   * JSON representation
+   * @returns {Object}
+   */
+  toJSON() {
+    return {
+      id: this.id,
+      username: this.username,
+      fullName: this.fullName,
+      isPrivate: this.isPrivate,
+      isVerified: this.isVerified,
+      isBusiness: this.isBusiness,
+      avatarURL: this.avatarURL,
+      biography: this.biography,
+      mediaCount: this.mediaCount,
+      followerCount: this.followerCount,
+      followingCount: this.followingCount,
+      lastSeen: this.lastSeen
+    };
+  }
 }
-
-module.exports = User
