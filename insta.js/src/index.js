f710e8d0d7985929d336712c8cf0211c9f67cb44
itxrex07 <itxrex07@gmail.com>
2025-07-27 12:41:46 +0500

    Update index.js

--- insta.js/src/index.js
+++ insta.js/src/index.js
@@ -1,17 +1,10 @@
-'use strict'
+export { default as Client } from './structures/Client.js';
+export { default as ClientUser } from './structures/ClientUser.js';
 
-module.exports = {
-    // Client
-    Client: require('./structures/Client'),
-    ClientUser: require('./structures/ClientUser'),
+export { default as Attachment } from './structures/Attachment.js';
+export { default as Chat } from './structures/Chat.js';
+export { default as Message } from './structures/Message.js';
+export { default as MessageCollector } from './structures/MessageCollector.js';
+export { default as User } from './structures/User.js';
 
-    // Structures
-    Attachment: require('./structures/Attachment'),
-    Chat: require('./structures/Chat'),
-    Message: require('./structures/Message'),
-    MessageCollector: require('./structures/MessageCollector'),
-    User: require('./structures/User'),
-
-    // Util
-    Util: require('./utils/Util')
-}
+export { default as Util } from './utils/Util.js';
