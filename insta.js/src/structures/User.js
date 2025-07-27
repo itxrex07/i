d1c10d6e37e95d608f4951dfcf405cba2edf15dd
itxrex07 <itxrex07@gmail.com>
2025-07-27 12:36:10 +0500

    Update User.js

--- insta.js/src/structures/User.js
+++ insta.js/src/structures/User.js
@@ -1,4 +1,4 @@
-const Collection = require('@discordjs/collection')
+import Collection from '@discordjs/collection';
 
 /**
  * Represents a User
@@ -241,4 +241,4 @@ class User {
     }
 }
 
-module.exports = User
+export default User;
